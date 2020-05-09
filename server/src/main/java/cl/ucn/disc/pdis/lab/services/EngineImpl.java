9448d80e421e009e455de9292e26a77c2d2eec99
Pablo-Castillo <pablo.castillo01@alumnos.ucn.cl>
2020-05-09 00:12:10 -0400

    -Corrected the documentation of the EngineImpl file at server/src/main/java/cl/ucn/disc/pdis/lab/services/EngineImpl.java

--- server/src/main/java/cl/ucn/disc/pdis/lab/services/EngineImpl.java
+++ server/src/main/java/cl/ucn/disc/pdis/lab/services/EngineImpl.java
@@ -10,6 +10,7 @@ package cl.ucn.disc.pdis.lab.services;
 
 import cl.ucn.disc.pdis.lab.zeroice.model.Engine;
 import com.zeroc.Ice.Current;
+import org.apache.commons.lang3.ObjectUtils;
 
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -30,59 +31,52 @@ public final class EngineImpl implements Engine {
     }
 
     /**
+     *Calculates the Dv of a given rut via Modulo 11 formula
+     *<p>
+     *The Module 11 formula consists in assign a check value to each digit of the base number
+     *starting from 2 for the less significative digit (the most to the right) to 7 for the most
+     *significative digit (the most to the left). If the base number has more than 6 numbers,
+     *then the secuence is restarted from 2. Finally, the addition of all the values is divided by 11
+     *and the remnants of that division is subtracted from 11 to get the check digit
      *
-     * @param rut
+     * @author Cristobal Diaz (formula) //cristobaldiaz.cl/blog/validacion-del-rut/
+     * @param rut The id of a person in chile
      * @param current The Current object for the invocation.
-     * @return
-     * @author http://cristobaldiaz.cl/blog/validacion-del-rut/ (formula)
+     * @return The dv of the rut
+     *
      */
     @Override
     public String getDigitoVerificador(String rut, Current current) {
 
-        /**
-         * Variable init.
-         */
+        //Variable initialize.
         char dv = 0;
         String digito = null;
         int mult = 0;
         int suma = 1;
 
         try {
-            /**
-             * rut uppercase just to work with an standard
-             */
-            rut =  rut.toUpperCase();
 
-            /**
-             * replacement of "." and "-" to get just alphanumeric characters
-             */
-            rut = rut.replace(".", "");
-            rut = rut.replace("-", "");
+           //rut uppercase just to work with an standard
+           rut =  rut.toUpperCase();
 
-            /**
-             * auxiliary string to resize the rut
-             */
-            int rutAux = Integer.parseInt(rut.substring(0, rut.length() - 1));
+           //replacement of "." and "-" to get just alphanumeric characters
+           rut = rut.replace(".", "");
+           rut = rut.replace("-", "");
 
-            /**
-             * gets the supposed dv from the last char of the string
-             */
+           //auxiliary string to resize the rut
+           int rutAux = Integer.parseInt(rut.substring(0, rut.length() - 1));
+
+           //gets the supposed dv from the last char of the string
             dv = rut.charAt(rut.length() - 1);
 
-            /**
-             * calculates de real Dv with the digits of the rut
-             */
+           //calculates de real Dv with the digits of the rut
             for (; rutAux != 0; rutAux /= 10) {
                 suma = (suma + rutAux % 10 * (9 - mult++ % 6)) % 11;
             }
-            /**
-             * checks the coincidence between the given DV and the calculated one
-             * if they match, returns the dv, else, sends a message of invalid dv
-             */
+           //checks the coincidence between the given DV and the calculated one
+           //if they match, returns the dv
             if (dv == (char) (suma != 0 ? suma + 47 : 75)) {
                 digito = String.valueOf(dv);
-            }else{
-                digito = "Invalid Dv";
             }
 
         } catch (java.lang.NumberFormatException e) {
